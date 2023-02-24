5da3c24c69e7b98c3a2f2f3829f5d64ad8a5a77f
Subhath <hasirasubath@gmail.com>
2023-02-24 22:40:44 +0530

    add price

--- models/productModel.js
+++ models/productModel.js
@@ -11,6 +11,11 @@ const productSchema = mongoose.Schema(
             required: true,
             default: 0
         },
+        price: {
+            type: Number,
+            required: true,
+            default: 0
+        },
         image: {
             type: String,
             required: false,
