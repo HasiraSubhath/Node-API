eb2904a2f7deca6878951c138c5279abb4fc26cf
Subhath <hasirasubath@gmail.com>
2023-02-24 23:58:44 +0530

    delete

--- server.js
+++ server.js
@@ -61,7 +61,22 @@ app.put('/products/:id', async(req, res) => {
         }
         const updatedProduct = await Product.findById(id);
         res.status(200).json(product);
-        
+
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//delete
+app.delete('/products/:id', async(req, res) =>{
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: 'cannot find any product with ID ${id}'})
+        }
+        res.status(200).json(product);
+
     } catch (error) {
         res.status(500).json({message: error.message})
     }
