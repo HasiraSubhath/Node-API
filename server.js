893635095735a4c70e3f0b554861e935c2090174
Subhath <hasirasubath@gmail.com>
2023-02-24 22:54:25 +0530

    fetch data

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const { json } = require('express')
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
@@ -14,7 +15,18 @@ app.get('/blog',(req, res) => {
     res.send('hi this is 2nd page')
 })
 
-app.post('/Product', async(req, res) =>{
+//Get data
+app.get('/products', async(req, res) =>{
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+//send data DB
+app.post('/Products', async(req, res) =>{
    try {
         const product = await Product.create(req.body)
         res.status(200).json(product);
